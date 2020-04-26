c0bccc2d16e7f4edf0eff0d4a91737d424aeb57d
Jaakko Pallari <jkpl@lepovirta.org>
2020-04-26 12:58:14 +0300

    Enhanced job trigger script

--- trigger_job.sh
+++ trigger_job.sh
@@ -1,13 +1,30 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # Trigger a pipeline job
 #
-set -eu
+set -euo pipefail
 
+# Project name
+IWOMPINAWS_PROJECT="${IWOMPINAWS_PROJECT:-"${1:-}"}"
+
+# Git branch
+GIT_BRANCH=${GIT_BRANCH:-"${2:-}"}
 if [ -z "${GIT_BRANCH:-}" ]; then
     GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 fi
 
+# Topic ARN
+IWOMPINAWS_TOPIC_ARN="${IWOMPINAWS_TOPIC_ARN:-"${3:-}"}"
+if [ -z "${IWOMPINAWS_TOPIC_ARN:-}" ]; then
+    IWOMPINAWS_TOPIC_ARN=$(\
+        aws cloudformation describe-stacks \
+            --stack-name iwomp \
+            --query 'Stacks[0].Outputs[?OutputKey==`JobTopicArn`].OutputValue' \
+            --output text \
+    )
+fi
+
+# Trigger!
 exec aws sns publish \
     --topic-arn "$IWOMPINAWS_TOPIC_ARN" \
     --message "{\"project\": \"$IWOMPINAWS_PROJECT\", \"branch\": \"$GIT_BRANCH\"}"
